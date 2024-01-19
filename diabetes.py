f8285a66032d2ed51b8936359fd9fe04aa8b695a
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-01-19 12:12:50 +0530

    Code Instructions

---
+++ diabetes.py
@@ -0,0 +1,29 @@
+# import pandas
+# import numpy 
+# import train_test_split
+# import logistic regression
+# import mean_squared error
+# import matplotlib
+# import seaborn
+# import standardscaler
+
+
+# read the dataset 
+
+# preprocessing steps :
+'''
+1. check for null values
+2. check for outliers
+3. check for categorical values
+4. check for correlation
+5. check for distribution of outcomes 
+6. check for co variance 
+7. check for skewness
+
+'''
+
+# remove outliers 
+# visualize various factors in the dataset 
+
+
+
